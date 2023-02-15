7d88b51960b3068e0890e0d093e9657f21ce59bb
allan <aejepsen@yahoo.com.br>
2023-02-15 10:22:20 -0300

    ok

--- computerStore/src/app/app.module.ts
+++ computerStore/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NaoEncontradoComponent } from './nao-encontrado/nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
