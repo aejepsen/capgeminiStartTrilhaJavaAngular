f320bffd7a520d23d68c87f6743964f87d132b73
allan <aejepsen@yahoo.com.br>
2023-02-18 09:35:38 -0300

    ok

--- computerStore/src/app/carrinho/carrinho.component.ts
+++ computerStore/src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { IProdutoCarrinho } from 'src/produtos';
 import { CarrinhoService } from '../carrinho.service';
 
@@ -9,17 +10,31 @@ import { CarrinhoService } from '../carrinho.service';
 })
 export class CarrinhoComponent implements OnInit {
   itensCarrinho: IProdutoCarrinho[] = [];
+  total = 0;
 
   constructor(
-    public carrinhoService: CarrinhoService
+    public carrinhoService: CarrinhoService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.itensCarrinho = this.carrinhoService.obterCarrinho();
+    this.calcularTotal();
+  }
+  
+  calcularTotal() {
+    this.total = this.itensCarrinho.reduce((prev, item) => prev + (item.preco * item.quantidade), 0);
   }
 
   removerProdutoCarrinho(produtoId: number) {
     this.itensCarrinho = this.itensCarrinho.filter(item => item.id !== produtoId);
     this.carrinhoService.removerProdutoCarrinho(produtoId);
+    this.calcularTotal();
 }
+
+  comprar() {
+    alert("Compra efetuada com sucesso !");
+    this.carrinhoService.limparCarrinho();
+    this.router.navigate(['produtos']);
+  }
 }
